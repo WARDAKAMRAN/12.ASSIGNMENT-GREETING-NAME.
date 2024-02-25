cd9bf98c9775a07a70253687ca11e0409f69b75b
Warda Kamran <wkamran523@gmail.com>
2024-02-24 23:20:57 -0800

    12.ASSIGNMENT GREETING NAME

---
+++ main.js
@@ -0,0 +1,9 @@
+// let members: string[] =['warda', 'waleed','aqsa','fathma','taimoor'];
+// for(let i=0; i<members.length; i++){
+//     console.log(members[i]);
+// }
+var members = ['warda', 'waleed', 'aqsa', 'fathma', 'taimoor'];
+var message = "kal birthday ha kis ke:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+// let members: string[] =['warda', 'waleed','aqsa','fathma','taimoor'];
+// for(let i=0; i<members.length; i++){
+//     console.log(members[i]);
+// }
+let members: string[] =['warda', 'waleed','aqsa','fathma','taimoor'];
+let message: string=`kal birthday ha kis ke:`
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+
+}
+
